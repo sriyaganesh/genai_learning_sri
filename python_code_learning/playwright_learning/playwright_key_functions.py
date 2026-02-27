f49feb995e1151c30a7e028779e19069e0296164
sriyaganesh <sriyaganesh@yahoo.com>
2026-02-27 22:36:52 +0530

    Playwright demo

--- python_code_learning/playwright_learning/playwright_key_functions.py
+++ python_code_learning/playwright_learning/playwright_key_functions.py
@@ -1,2 +1,23 @@
 from playwright.async_api import async_playwright
+import asyncio
+
 print("Playwright key functions demo")
+
+async def playwright_key_function():
+    async with async_playwright() as p:
+        browser=await p.chromium.launch(headless=False) #browser will be launched
+        page=await browser.new_page() #new page will be opened
+        #navigation
+        await page.goto("https://www.google.com/")
+        await page.wait_for_timeout(3000)
+        await browser.close() #browser will be closed
+
+        #css selector
+       # Step 1 f12+ or right click +inspect
+       # Step 2 element click
+         # Step 3 right click copy and select copy selector or copy xpath
+         
+
+
+if __name__ == "__main__":
+    asyncio.run(playwright_key_function())
