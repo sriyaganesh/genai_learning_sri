98e0b0907b199e2ff0ee4d004cf6c8be98fedcbe
sriyaganesh <sriyaganesh@yahoo.com>
2026-03-05 00:17:15 +0530

    ai insight

---
+++ python_code_learning/model_import_learning/openai_closed_llm.py
@@ -0,0 +1,30 @@
+--open ai closed llm
+
+import streamlit as st
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+# ---------------------------
+# CONFIG
+# ---------------------------
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+st.title("Closed LLM Assistant")
+
+user_input = st.text_area("Ask something")
+
+if st.button("Submit"):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful enterprise AI assistant."},
+            {"role": "user", "content": user_input}
+        ],
+        temperature=0.3
+    )
+
+    st.write(response.choices[0].message.content)
