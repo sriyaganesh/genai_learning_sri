1fde6d5da6398ac2f61dd33b105a176a0aed4c18
sriyaganesh <sriyaganesh@yahoo.com>
2026-03-05 00:05:50 +0530

    new

---
+++ python_code_learning/model_import_learning/model_learning.py
@@ -0,0 +1,8 @@
+
+from transformers import pipeline
+
+generator=pipeline("text-generation", model="gpt2")
+
+result=generator("Once upon a time", max_length=50, num_return_sequences=1)
+
+print(result[0]["generated_text"])
